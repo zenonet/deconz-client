12e34d05b729f5bb8b74a0e9801c2c3f887a04e0
zenonet <git@zenonet.de>
2025-06-16 16:09:07 +0200

    Organizing, making toggle button indicate current state, only show control panel when light is selected

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -14,7 +14,7 @@ use tokio::runtime::Runtime;
 
 struct ViewModel {
     state: Mutex<State>,
-    client: DeconzClient
+    client: DeconzClient,
 }
 
 struct State {
@@ -34,19 +34,23 @@ impl ViewModel {
         let url = env::var("DECONZ_URL").expect("Missing DECONZ_URL in env vars");
         let token = env::var("DECONZ_TOKEN").expect("Missing DECONZ_TOKEN in env vars");
         ViewModel {
-            state: Mutex::new(State{
+            state: Mutex::new(State {
                 lights: vec![],
                 selected_index: usize::MAX,
                 selected_light_state: None,
             }),
-            client: DeconzClient::login_with_token(url, token).expect("Failed to connect to deconz server")
+            client: DeconzClient::login_with_token(url, token)
+                .expect("Failed to connect to deconz server"),
         }
     }
 }
 
-
-struct Ui{
-    list_box: ListBox
+struct Ui {
+    list_box: ListBox,
+    toggle_button: Button,
+    light_name_label: Label,
+    toggle_button_text: Label,
+    controller_layout: gtk::Box,
 }
 
 fn runtime() -> &'static Runtime {
@@ -54,42 +58,54 @@ fn runtime() -> &'static Runtime {
     RUNTIME.get_or_init(|| Runtime::new().expect("Setting up tokio runtime needs to succeed."))
 }
 
-fn build_ui(application: &gtk::Application) {
+fn build_ui(application: &gtk::Application) -> Ui {
     let window = gtk::ApplicationWindow::new(application);
 
     window.set_title(Some("Deconz Control"));
     window.set_default_size(500, 700);
 
-    let model = Arc::new(ViewModel::init());
-    
     let list_box = gtk::ListBox::new();
-    
+
     let scrolled_window = ScrolledWindow::builder().child(&list_box).build();
-    
-    let controller = gtk::Box::new(Orientation::Vertical, 10);
+
+    let controller_layout = gtk::Box::new(Orientation::Vertical, 10);
+    controller_layout.set_visible(false);
     let light_name_label = Label::new(Some("No lamp selected"));
-    
-    controller.append(&light_name_label);
-    let l = Label::new(Some("Toggle lamp"));
+
+    controller_layout.append(&light_name_label);
+    let toggle_button_text = Label::new(Some("Toggle lamp"));
     let toggle_button = Button::builder()
-    .child(&l)
+        .child(&toggle_button_text)
         .tooltip_text("Toggles the on/off state of the lamp")
         .build();
-    controller.append(&toggle_button);
-    
+    controller_layout.append(&toggle_button);
+
     let layout = gtk::Box::new(gtk4::Orientation::Horizontal, 0);
     layout.set_homogeneous(true);
-    
-    layout.append(&scrolled_window);
-    layout.append(&controller);
-    
-    window.set_child(Some(&layout));
-    
-    let ui = Arc::new(Ui{
-        list_box,
-    });
 
-    fn fetch_light_state(model: Arc<ViewModel>){
+    layout.append(&scrolled_window);
+    layout.append(&controller_layout);
+
+    window.set_child(Some(&layout));
+
+    let ui = Ui {
+        list_box,
+        toggle_button,
+        light_name_label,
+        toggle_button_text,
+        controller_layout,
+    };
+
+    window.present();
+
+    ui
+}
+
+fn add_app_logic(ui: Ui) {
+    let model = Arc::new(ViewModel::init());
+    let ui = Arc::new(ui);
+
+    fn fetch_light_state(model: Arc<ViewModel>, ui: Arc<Ui>){
         glib::spawn_future_local(async move {
             let mut state = model.state.lock().unwrap();
             if let Some(light) = state.selected_light() {
@@ -100,15 +116,15 @@ fn build_ui(application: &gtk::Application) {
                     .expect(&format!("Failed to load state of light {}", light.name));
                 println!("Got state for {}:\n{:#?}", light.name, light_state);
                 state.selected_light_state = Some(light_state);
+                ui.controller_layout.set_visible(true);
+                ui.toggle_button_text.set_text(if light_state.on {"Turn off"} else {"Turn on"});
             }
-            println!("Completed light state init");
         });
     }
-    
-    
+
     let fetch_light_list = {
         let ui = ui.clone();
-         move |model: Arc<ViewModel>|{
+        move |model: Arc<ViewModel>| {
             glib::spawn_future_local(async move {
                 let light_list = model.client.get_light_list().await.unwrap();
 
@@ -125,12 +141,13 @@ fn build_ui(application: &gtk::Application) {
 
     {
         let model = model.clone();
+        let a_ui = ui.clone();
         ui.list_box.connect_row_selected(move |_, row| {
             if let Some(row) = row {
                 let label = row.child().unwrap().downcast::<Label>().unwrap();
                 println!("Row {} was selected", label.text());
-                light_name_label.set_text(&label.text());
-                
+                a_ui.light_name_label.set_text(&label.text());
+
                 let mut state = model.state.lock().unwrap();
 
                 // Find the selected light:
@@ -140,14 +157,15 @@ fn build_ui(application: &gtk::Application) {
                 state.selected_index = light;
 
                 // Load current light state
-                fetch_light_state(model.clone());
+                fetch_light_state(model.clone(), a_ui.clone());
             }
         });
     }
 
     {
         let model = model.clone();
-        toggle_button.connect_clicked(move |_| {
+        let a_ui = ui.clone();
+        ui.toggle_button.connect_clicked(move |_| {
             let state = &model.state.lock().unwrap();
 
             let Some(light_state) = state.selected_light_state else {
@@ -157,9 +175,12 @@ fn build_ui(application: &gtk::Application) {
 
             {
                 let model = model.clone();
+                let ui = a_ui.clone();
                 glib::spawn_future_local(async move {
-                    model.client
-                        .set_on_state( // TODO: Only give the light id so that the mutex is not locked while the request is sent
+                    model
+                        .client
+                        .set_on_state(
+                            // TODO: Only give the light id so that the mutex is not locked while the request is sent
                             &*model.state.lock().unwrap().selected_light().unwrap(),
                             new_on_state,
                         )
@@ -167,22 +188,27 @@ fn build_ui(application: &gtk::Application) {
                         .unwrap();
 
                     // Update light state
-                    fetch_light_state(model);
+                    fetch_light_state(model, ui.clone());
                 });
             }
         });
     }
 
-    window.present();
-
     fetch_light_list(model);
 }
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let application = gtk::Application::builder()
         .application_id("de.zenonet.deconz")
         .build();
-    application.connect_activate(build_ui);
+
+    fn init(app: &gtk::Application) {
+        let ui = build_ui(&app);
+        add_app_logic(ui);
+    }
+
+    application.connect_activate(init);
     application.run();
 
     Ok(())
